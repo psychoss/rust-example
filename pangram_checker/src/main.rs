82dc4bb2231e111a3dbaa509ac80b50dbc31a67b
psychoss <626377748@qq.com>
2015-12-19 19:22:23 +0800

    changed

---
+++ pangram_checker/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let characters: Vec<char> = "The quick brown fox jumps over the lazy dog".chars().collect();
+    let mut mark: [bool; 26] = [false; 26];
+    let ia = 'a' as i32;
+    let iA = 'A' as i32;
+    let mut h = -1;
+    let mut count = 0;
+    for c in characters {
+        if c >= 'a' {
+            if c > 'z' {
+                continue;
+            }
+            h = (c as i32) - ia;
+
+
+        } else {
+            if c < 'A' || c > 'Z' {
+                continue;
+            }
+            h = (c as i32) - iA;
+        }
+        if !mark[h as usize] {
+            
+            if count == 25 {
+                println!("The statement is pangarm.");
+                return;
+            }
+            mark[h as usize] = true;
+            count = count + 1;
+
+        }
+
+    }
+    println!("The statement is not pangarm.")
+}
