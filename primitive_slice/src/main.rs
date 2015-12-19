fe0515b70040001605a4427dc474ffeae66d1377
psychoss <626377748@qq.com>
2015-12-19 22:36:11 +0800

    changed

--- primitive_slice/src/main.rs
+++ primitive_slice/src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
+	is_empty();
     len();
+    swap();
 }
 
-fn len() {
+fn is_empty() {
+    let a = [1, 2, 3];
+    println!("is a empty slice? {}", a.is_empty());
+}
+
+
+
+fn len(){
     let a = [1, 2, 3];
     println!("the number of elements in the slice is {}", a.len());
+	
+}
+
+fn swap(){
+	 let mut a = [1, 2, 3];
+	 a.swap(0,1);
+    println!("after swap {:?}",a );
 }
