4abfefa12ed7745c99742ca5f41ac1059d25f725
psychoss <626377748@qq.com>
2015-12-20 19:25:58 +0800

    changed

--- search_a_list/src/main.rs
+++ search_a_list/src/main.rs
@@ -1,7 +1,7 @@
-fn main() {
-    let haystack = vec!["A", "B", "E", "F", "A"];
-    let mut h_ite = haystack.iter();
-    println!("{}", h_ite.position(|s| *s == "A").unwrap());
-    println!("{}", h_ite.rposition(|s| *s == "A").unwrap());
+fn main(){
+    let haystack=vec!["B","A","B","E","F","A"];
 
+    println!("{}",haystack.iter().position(|s| *s=="A").unwrap());
+    println!("{}",haystack.iter().rposition(|s| *s=="A").unwrap());
+    
 }
